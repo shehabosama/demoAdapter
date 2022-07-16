b17d050cecad0a3e25820f2a3d8637d0f8ca6d53
shehabosama <shehab.osama.eg@gmail.com>
2022-07-16 19:38:38 +0200

    Revert "this is our forth commit"
    This reverts commit 5504a56b

--- app/src/main/java/com/example/gittutorial/MainActivity.kt
+++ app/src/main/java/com/example/gittutorial/MainActivity.kt
@@ -9,6 +9,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         println("this is contained in my second commit")
-        slkdjflksjadfsdf
+
+        today is a good day
     }
 }
