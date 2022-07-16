afe9f888e73d7a6c00106143c9d9f3932b94f8c1
shehabosama <shehab.osama.eg@gmail.com>
2022-07-16 21:25:28 +0200

    this is second commit from Ecperimental branch

--- app/src/main/java/com/example/gittutorial/MainActivity.kt
+++ app/src/main/java/com/example/gittutorial/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
 
         println("this is contained in my second commit")
         println("local change")
-        println("CHANGE 2")
+        println("CHANGE second")
         println("these changes will marge from experimantl to main branch")
     }
 }
